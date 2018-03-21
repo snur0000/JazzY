971c9abdbb1b3825fb7f49a2fc868b5575af9ed5
Azeedo <azeedo@users.noreply.github.com>
2018-03-21 14:11:08 -0400

    Update on SQL tables
    I change a few of the columns name to better fit the application needs.

--- database/Rebuilddb.sql
+++ database/Rebuilddb.sql
@@ -1,56 +1,39 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- * Author:  Owner
- * Created: Feb 9, 2018
- */
-
-drop table OrderItems;
+drop table OrderDetails;
 drop table Orders;
 drop table Customers;
 drop table Tickets;
 
-CREATE TABLE Tickets
-	(TicketSection INTEGER,
-	Cost INTEGER,
-	MaxAvailable INTEGER,
-	PRIMARY KEY(TicketSection)
-	);
-	
 CREATE TABLE Customers
-	(CustomerID  INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1000, INCREMENT BY 1),
+	(CustomerID  INTEGER,
 	CustLastName VARCHAR(10) NOT NULL,
 	CustFirstName VARCHAR(10) NOT NULL,
-	CustAddress VARCHAR(20),
-	CustCity VARCHAR(12),
-	CustState VARCHAR(2),
-	CustZip VARCHAR(5),
-	CustEmail VARCHAR(30),
+	CustAddress VARCHAR(20)NOT NULL,
+	CustCity VARCHAR(12)NOT NULL,
+	CustState VARCHAR(2)NOT NULL,
+	CustZip VARCHAR(5)NOT NULL,
+	CustEmail VARCHAR(30)NOT NULL,
 	PRIMARY KEY(CustomerID)
 	);
 
+CREATE TABLE Tickets
+	(TicketNumber INTEGER,
+     TicketSection INTEGER,
+	 TicketCost INTEGER,
+	 MaxAvailable INTEGER,
+	PRIMARY KEY(TicketNumber)
+	);
+	
 CREATE TABLE Orders
-	(OrderID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 100, INCREMENT BY 1),
+	(OrderID INTEGER,
 	CustomerID INTEGER references Customers(CustomerID),
-	OrderDate DATE NOT NULL,
-	ShipCost FLOAT,
-	AquisitionFee FLOAT,
-	PRIMARY KEY(OrderID)
-	);
-
-CREATE TABLE ORDERITEMS
-	(OrderID INTEGER references Orders(OrderID),
-	 ItemNumber INTEGER,
-	 TicketSection INTEGER references Tickets(TicketSection),
-	 Quantity INTEGER,
-	 PRIMARY KEY (OrderID, ItemNumber)
-	);
-
---REFERENCE TABLE VALUES--
-    INSERT INTO Tickets VALUES (1,50,75);
-    INSERT INTO Tickets VALUES (2,40,75);
-    INSERT INTO Tickets VALUES (3,30,75);
-          
+	OrderDate DATE default sysdate,
+	ShipCost NUMBER(8,2) DEFAULT '0.00',
+	AquisitionFee NUMBER(8,2) DEFAULT '0.00',
+	OrderTotal as shipCost + aquisitionfee + ticket cost, 
+	PRIMARY KEY(OrderID));
+  
+CREATE TABLE OrderDetails
+	 (OrderID INTEGER references Orders,
+	 TicketNumber INTEGER references TICKETS,
+	 TicketQty INTEGER,
+	 PRIMARY KEY (OrderID, TicketNumber));
